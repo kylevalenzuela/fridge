c850a22d8846af5e8e4ebb774247b597e3934512
Kyle Valenzuela <valenzuela.kyle@gmail.com>
2016-08-13 16:03:19 -0700

    changing gpio

--- fridge.py
+++ fridge.py
@@ -2,15 +2,15 @@ import RPi.GPIO as GPIO #import the GPIO library
 import time
 
 GPIO.setmode(GPIO.BOARD)  
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 name = "Kyle"  
 print("Hello " + name)
 
 while True:  
-    if GPIO.input(23):
+    if GPIO.input(8):
        print("Door is open")
        time.sleep(2)
-    else:
+    if (GPIO.input(8) == False:
        print("Door is closed")
        time.sleep(2)
