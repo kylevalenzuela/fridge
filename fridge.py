5abb527bf48f8906fe6ef567ef37baecb58ac51f
Kyle Valenzuela <valenzuela.kyle@gmail.com>
2016-08-13 15:43:01 -0700

    First commit

---
+++ fridge.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO #import the GPIO library  
+import time
+
+GPIO.setmode(GPIO.BOARD)  
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+name = "Kyle"  
+print("Hello " + name)
+
+while True:  
+    if GPIO.input(23):
+       print("Door is open")
+       time.sleep(2)
+    if (GPIO.input(23) == False:
+       print("Door is closed")
+       time.sleep(2)
